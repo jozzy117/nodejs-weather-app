7d196c488e995b0a80ea5cae1342b5c3df267392
Jonathan Ojakovo <ojakovojo@gmail.com>
2020-10-24 10:57:18 +0100

    Forecast daily min and max temp

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,7 +4,7 @@ const forecast = (lon, lat, callback)=>{
     const url = 'http://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&appid=77e18dc8ff9ccb2a1b5614788fdffbf2&units=metric'
     got({url, responseType:'json'})
     .then(({body})=>{
-        callback( body.current.weather[0].description + ', the current temperature is ' + body.current.temp + ' degree. There is a ' + body.daily[0].pop + ' percent chance of rain.', undefined)
+        callback( body.current.weather[0].description + '! The current temperature is ' + body.current.temp + ' degree, with the daily temperature ranging from '+ body.daily[0].temp.min + ' - ' + body.daily[0].temp.max +' degree. There is a ' + body.daily[0].pop + ' percent chance of rain.', undefined)
     })
     .catch((error)=>{
         if (error.response){
